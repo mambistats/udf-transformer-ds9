c413d4b0516554481f835fb924a7335ee7a370dc
kc14 <frank.kemmer@gmail.com>
2017-10-02 00:35:54 +0200

    Added script to create a range file from free GeoLite2 City CSV data

--- scripts/test-udf-transformer-one-line.sh
+++ scripts/test-udf-transformer-one-line.sh
@@ -3,5 +3,5 @@
 echo $'a\tb\tc\t127.126.125.124\t223.123.124.129\tf\tg\th\ti\tj\tk\tl\tm\tn\to\tp\tq\tr' | \
 java -Xmx4G -cp "${BASEDIR}/target/udf-transformer-ds9-0.0.1-SNAPSHOT-jar-with-dependencies.jar" \
 Use Basic,IPv6 \
---ipv6-ranges "${BASEDIR}/resources/geoip2_city_ipv6_network_ranges-pdate=2017-09-04.gz" \
+--ipv6-ranges "${BASEDIR}/resources/ipv6+4_network_ranges.gz" \
 --select 1 2 3 4 5 IPv6.ip[4] IPv6.ip[5] 6 7 8 9 10 11 12 13 14 15 16 17 Basic.concat[1,2] Basic.concat_v[6,7,8]
