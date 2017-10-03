2fa5a565931eff7f9d8e1fc8ddb27c06efbd12f2
kc14 <frank.kemmer@gmail.com>
2017-10-04 00:23:03 +0200

    curl is not working ... somehow it only download 32 k of the 100 k file

--- scripts/get-udger-data.sh
+++ scripts/get-udger-data.sh
@@ -1,6 +1,3 @@
 #! /usr/bin/env bash
 cd "${BASEDIR}/resources"
 curl "https://github.com/udger/test-data/blob/master/data_v3/udgerdb_v3.dat" -o udgerdb_v3.dat
-
-echo "Warning: the current test data is too old and DOES NOT WORK!"
-echo "You need a commercial uptodate [udgerdb_v3.dat] database"
