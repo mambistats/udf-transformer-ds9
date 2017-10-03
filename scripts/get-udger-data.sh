e65b348c4641c48bae7f5e310e59985b9192a562
kc14 <frank.kemmer@gmail.com>
2017-10-04 00:48:29 +0200

    Added -L to follow the redirect send by GitHub

--- scripts/get-udger-data.sh
+++ scripts/get-udger-data.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 cd "${BASEDIR}/resources"
-curl "https://github.com/udger/test-data/blob/master/data_v3/udgerdb_v3.dat" -o udgerdb_v3.dat
+curl -L -H "Accept: application/octet-stream" "https://github.com/udger/test-data/raw/master/data_v3/udgerdb_v3.dat" -o udgerdb_v3.dat
+echo 'Done'
